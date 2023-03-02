c3234874b5822bbd4db5815d951717b65e567a96
LI3L <khtkvg@gmail.com>
2023-03-02 09:09:46 +0200

    Create lesson 2.js

---
+++ lesson 2.js
@@ -0,0 +1,19 @@
+//Functions
+// (place variables) => {right the function here}
+const sayHello = () => {
+    console.log("Hello from java functuon");
+}
+sayHello();
+
+//get pareters
+const addNumbers = (num1,num2) =>{
+    console.log(num1+num2);
+}
+addNumbers(3,4);
+
+const sayMayName =(name,age)=>{
+    return `My name is ${name} and i am ${age}`
+}
+
+const result = sayMayName("liel",20);
+console.log(result);
